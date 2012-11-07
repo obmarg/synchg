f4bed36a5f22d8e0660e66439e79a33bcce16494
Graeme Coupar <grambo@grambo.me.uk>
2012-11-07 13:36:26 +0000

    Updated version numbers and changelog for v0.9.6

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 
 setup(
     name='SyncHg',
-    version='0.9.5',
+    version='0.9.6',
     url='http://synchg.readthedocs.org/',
     license='BSD',
     author='Graeme Coupar',
